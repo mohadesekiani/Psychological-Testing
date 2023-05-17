aface3aa46e363e4569f098dcb25e57de9b259ab
mohadesekiani <mohadeskiani6@gmail.com>
2023-05-17 11:15:56 +0430

    setting module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './home/home.component';
 import {  RouterModule } from '@angular/router';
-
+import { LayoutModule } from './layout/layout.module';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { PsychologicalTestComponent } from './psychological-test/psychological-test.component';
+import { MaterialModule } from './shared/elements/material/material.module';
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    NotFoundComponent,
+    PsychologicalTestComponent,
+    
   ],
   imports: [
+    LayoutModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     RouterModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
