aab66e4fafbc1422654358d1cba7a67cbff8136a
mohadesekiani <mohadeskiani6@gmail.com>
2023-05-17 11:15:06 +0430

    created routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LayoutComponent } from './layout/layout.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { PsychologicalTestComponent } from './psychological-test/psychological-test.component';
 
 const routes: Routes = [
-  { path:'',component:HomeComponent }
+  { path:'',component:LayoutComponent ,
+    children:[
+    { path: '', component: HomeComponent },
+    { path: ':id', component: PsychologicalTestComponent },
+
+    ]
+  },
+
+
+
+
+
+  //اینو حتما باید اخرین مورد بنویسید
+  {path:'not_found', component:NotFoundComponent},
+  {path: '**',redirectTo: 'not_found'}
 ];
 
 @NgModule({
