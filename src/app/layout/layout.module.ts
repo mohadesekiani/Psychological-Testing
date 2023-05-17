0b49db24369d83a513635ecc9665abb3bc51094c
mohadesekiani <mohadeskiani6@gmail.com>
2023-05-17 12:05:07 +0430

    created layout componet

---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,24 @@
+import { Input, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
+import { LayoutComponent } from './layout.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HomeComponent } from '../home/home.component';
+
+import {  RouterModule } from '@angular/router';
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    SidebarComponent,FooterComponent,LayoutComponent],
+  imports: [
+    CommonModule,
+    SharedModule,
+    RouterModule
+  ]
+})
+export class LayoutModule { 
+
+}
