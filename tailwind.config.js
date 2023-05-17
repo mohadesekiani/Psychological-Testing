f67b1fc09b44b02500220c4d13280c1fcf544b82
mohadesekiani <mohadeskiani6@gmail.com>
2023-05-17 11:13:05 +0430

    config tailwind for style footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+
+    theme: {
+      extend: {
+        colors: {
+          'custom-footer': '#243c5a',    
+        },
+      },
+    },
+
   content: [
     "./src/**/*.{html,ts}",
   ],
-  theme: {
-    extend: {},
-  },
+
   plugins: [],
 }
