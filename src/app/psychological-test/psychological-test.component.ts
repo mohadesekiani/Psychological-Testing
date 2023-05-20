c254dabaf51d17d59fb829bd44e9195d61fdd2a8
mohadesekiani <mohadeskiani6@gmail.com>
2023-05-20 17:13:35 +0430

    created psychological component

--- src/app/psychological-test/psychological-test.component.ts
+++ src/app/psychological-test/psychological-test.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { IPsychological } from '../core/shema/models/Ipsychological'
+// import { ActivatedRoute } from '@angular/router';
+// import { IPsychological } from '../core/shema/models/Ipsychological'
 @Component({
   selector: 'app-psychological-test',
   templateUrl: './psychological-test.component.html',
@@ -8,33 +8,29 @@ import { IPsychological } from '../core/shema/models/Ipsychological'
 })
 export class PsychologicalTestComponent {
 
-  constructor(private route:ActivatedRoute){}
+  // constructor(private route:ActivatedRoute){}
 
-  selectedId:number;
-  selectedTest?:IPsychological;
-  PsychologicalTests:IPsychological[]=[
-  {  
-    id:1,
-    question:["سوال 1","سوال 2"],
-    title:"تست هوش ",
-  },
-  { 
-     id:2,
-    question:["سوال 1","سوال 2"],
-    title:"تست هوش ",
-  },
-  { 
-     id:3,
-    question:["سوال 1","سوال 2"],
-    title:"تست هوش ",
-  }
-]
+  // selectedId:number;
+  // selectedTest?:IPsychological;
+//   PsychologicalTests:IPsychological[]=[
+//   {  
+//     id:1,
+//     questions:["سوال 1","سوال 2"],
+//     title:"تست هوش ",
+//   },
+//   { 
+//      id:2,
+//     question:["سوال 1","سوال 2"],
+//     title:"تست هوش ",
+//   },
+
+// ]
 ngOnInit(): void {
 
-    this.selectedId  = +this.route.snapshot.params['id'];
+    //this.selectedId  = +this.route.snapshot.params['id'];
     //const queryParams = this.route.snapshot.queryParams;
-    console.log(this.selectedId);
-    this.selectedTest = this.PsychologicalTests.find(test => test.id === this.selectedId)
+   // console.log(this.selectedId);
+    // this.selectedTest = this.PsychologicalTests.find(test => test.id === this.selectedId)
 
 
   }
