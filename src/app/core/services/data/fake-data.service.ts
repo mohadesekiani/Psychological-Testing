56b7b2ffc07134b73e2129d0dc18436f84ab3380
mohadesekiani <mohadeskiani6@gmail.com>
2023-05-20 17:12:46 +0430

    created fakedata

--- src/app/core/services/data/fake-data.service.ts
+++ src/app/core/services/data/fake-data.service.ts
@@ -1,17 +1,30 @@
 import { Injectable } from '@angular/core';
-import * as fakeData from './fake-data'
-
+import * as fakeData from './mock-data'
+import { IPsychological } from '../../shema/models/Ipsychological';
+import { AbFakeDataService } from './ab-fake-data.service';
+import { Iauth } from '../../shema/models/iauth';
 @Injectable({
   providedIn: 'root'
 })
-export class FakeDataService {
+export class FakeDataService extends AbFakeDataService{
 
-  constructor() { 
-    
-  }
 
-  getFakeData(): any {
+  public getFakedata(): Array<IPsychological> {
     return fakeData.PsychologicalTests;
   }
+  public login(user:Iauth): boolean {
+    if (user.username === fakeData.user.username && user.password === fakeData.user.password) {
+      console.log(user,true);
+      
+      
+      // // اطلاعات ورود صحیح است
+       return true;
+    } else {
+      console.log(user,false);
 
+      // اطلاعات ورود نامعتبر است
+      return false;
+    }
+  }
+   
 }
