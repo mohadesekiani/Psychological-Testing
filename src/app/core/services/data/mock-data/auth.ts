c1da4751421e438d38efbd8c1c77b553e1b53847
mohadesekiani <mohadeskiani6@gmail.com>
2023-05-20 17:14:29 +0430

    created mockData

---
+++ src/app/core/services/data/mock-data/auth.ts
@@ -0,0 +1,15 @@
+import { Iauth } from "src/app/core/shema/models/iauth";
+// export let  Auth:Array<Iauth> = [
+//     {
+//         username:"mohadese",
+//         password:"kiani1026"
+//     }
+// ]  
+// export let username = 'admin';
+// export let password = 'password';
+
+
+export let user: Iauth = {
+  username: "admin",
+  password: "password123"
+};
