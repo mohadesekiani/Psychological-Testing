637c5b4543a873aaf663dbf6512d8d14580cce39
mohadesekiani <mohadeskiani6@gmail.com>
2023-05-20 17:19:04 +0430

    home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FakeDataService } from '../core/services/data/fake-data.service';
 import { IPsychological } from '../core/shema/models/Ipsychological';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
-  items:IPsychological [];
+export class HomeComponent  {
+  items: IPsychological[];
 
-  constructor(private fakeDataService: FakeDataService) {
-    this.items = this.fakeDataService.getFakeData();
-    
+  constructor(private fakeDataService: FakeDataService, private route: ActivatedRoute,private router: Router) {
+    this.items = this.fakeDataService.getFakedata();
   }
+
+  selectedTest(id: number) {
+      this.router.navigate(['/psychologicalTest/', id]); // صفحه مورد نظر را با آیدی مشخص هدایت می‌کند
+     }
+
+
+  
 }
