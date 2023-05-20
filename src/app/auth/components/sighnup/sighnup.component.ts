1a3163e5727f6f1f39ea3971bf6770de7285e47f
mohadesekiani <mohadeskiani6@gmail.com>
2023-05-20 17:10:32 +0430

    sighnup component

---
+++ src/app/auth/components/sighnup/sighnup.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sighnup',
+  templateUrl: './sighnup.component.html',
+  styleUrls: ['./sighnup.component.scss']
+})
+export class SighnupComponent {
+
+}
