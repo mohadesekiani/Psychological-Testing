be10184b08bb21008c45915b45908e07366cbb91
mohadesekiani <mohadesekiani6@gmail.com>
2023-05-21 11:41:53 +0430

    edit model sighnup

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,18 +1,21 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
- import { LoginComponent } from './components/login/login.component';
-import { AuthRoutingModule } from './auth-routing.module';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { RouterModule } from '@angular/router';
 import { FormlyModule } from '@ngx-formly/core';
 import { FormlyMaterialModule } from '@ngx-formly/material';
-import { ReactiveFormsModule } from '@angular/forms';
-import {  RouterModule } from '@angular/router';
+
+import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { LoginComponent } from './components/login/login.component';
+import { SighnupComponent } from './components/sighnup/sighnup.component';
 
 @NgModule({
-  declarations: [ 
+  declarations: [
     AuthComponent,
-     LoginComponent,
+    LoginComponent,
+    SighnupComponent,
   ],
   imports: [
 
